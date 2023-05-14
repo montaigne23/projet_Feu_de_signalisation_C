e9dc94f7cb06f6fd08ab0eeadf5c2b018bfdbfb8
montaigne23 <www.landau.com@gmail.com>
2023-05-14 16:21:03 +0200

    update

--- src/include/customs/feu_de_signalisation1.h
+++ src/include/customs/feu_de_signalisation1.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <SDL2/SDL.h>
-int p1 = 1;
+int p1 = 20;
 int feu_de_signalisation1(SDL_Renderer *renderer, int x, int y, int h, int l, int x_feu1, int y_feu1, int x_feu2, int y_feu2, int x_feu3, int y_feu3)
 {
     SDL_Rect pt1 = {443 + x + x_feu1, 265 + y + y_feu1, 25, 25};
@@ -11,33 +11,35 @@ int feu_de_signalisation1(SDL_Renderer *renderer, int x, int y, int h, int l, in
     SDL_Rect dff = {441 + x, 261 + y, l, h};
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderFillRect(renderer, &dff);
-    int p_ = p1;
-    switch (p1)
+
+    if (p1 <= 15)
     {
-    case p_ < = 15:
         // feu vert1
         SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
         SDL_RenderFillRect(renderer, &pt1);
         SDL_RenderPresent(renderer);
         p1++;
-        break;
-    case p1 >= 15 & p1 >= 20:
+    }
+    else if (p1 > 15 & p1 <= 20)
+    {
         // feu orange1
         SDL_SetRenderDrawColor(renderer, 255, 127, 40, 255);
         SDL_RenderFillRect(renderer, &pt3);
         SDL_RenderPresent(renderer);
         p1++;
-        break;
-    case p1 >= 20 & p1 <= 35:
+    }
+    else if (p1 > 20 & p1 <= 35)
+    {
         // feu rouge1
         SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
         SDL_RenderFillRect(renderer, &pt2);
         SDL_RenderPresent(renderer);
-        p1 = 1;
-        break;
-    default:
-        break;
+        p1++;
     }
+        if (p1 == 35)
+        {
+            p1 = 0;
+        }
 
     // Sombre
     //  SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
--- src/include/customs/feu_de_signalisation2.h
+++ src/include/customs/feu_de_signalisation2.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <SDL2/SDL.h>
-int p2 = 1;
+int p2 = 0;
 int feu_de_signalisation2(SDL_Renderer *renderer, int x, int y, int h, int l, int x_feu1, int y_feu1, int x_feu2, int y_feu2, int x_feu3, int y_feu3)
 {
     SDL_Rect pt1 = {443 + x + x_feu1, 265 + y + y_feu1, 25, 25};
@@ -12,31 +12,33 @@ int feu_de_signalisation2(SDL_Renderer *renderer, int x, int y, int h, int l, in
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderFillRect(renderer, &dff);
 
-    switch (p2)
+    if (p2 <= 15)
     {
-    case 1:
         // feu vert1
         SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
         SDL_RenderFillRect(renderer, &pt1);
         SDL_RenderPresent(renderer);
         p2++;
-        break;
-    case 2:
+    }
+    else if (p2 > 15 & p2 <= 20)
+    {
         // feu orange1
         SDL_SetRenderDrawColor(renderer, 255, 127, 40, 255);
         SDL_RenderFillRect(renderer, &pt3);
         SDL_RenderPresent(renderer);
         p2++;
-        break;
-    case 3:
+    }
+    else if (p2 > 20 & p2 <= 35)
+    {
         // feu rouge1
         SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
         SDL_RenderFillRect(renderer, &pt2);
         SDL_RenderPresent(renderer);
-        p2 = 1;
-        break;
-    default:
-        break;
+        p2++;
+    }
+    if (p2 == 35)
+    {
+        p2 = 0;
     }
 
     // Sombre
